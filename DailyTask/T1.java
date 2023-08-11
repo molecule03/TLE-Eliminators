22ada97891a6401abcacc1d85b3e636af87db564
molecule03 <laveshsalaskar619@gmail.com>
2023-08-11 23:58:23 +0530

    Upsolved

--- DailyTask/T1.java
+++ DailyTask/T1.java
@@ -1,4 +1,27 @@
 package TLE.DailyTask;
 
+import java.util.Scanner;
+
 public class T1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String p = sc.next();
+
+        int count = 0;
+
+        int l = 0;
+        int r = p.length();
+
+        while(r<=s.length()){
+
+            String cur = s.substring(l, r);
+            if(cur.equals(p)) count++;
+            l++;
+            r++;
+        }
+
+        System.out.println(count);
+    }
 }
--- DailyTask/T2.java
+++ DailyTask/T2.java
@@ -1,4 +1,8 @@
 package TLE.DailyTask;
 
+import  java.util.*;
+
 public class T2 {
+
+
 }
